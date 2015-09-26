8c735c8e4e50d9a3f3eeafbdbcb123c39a0829fa
mnandrei <mnandrei90@gmail.com>
2015-09-26 18:53:15 +0300

    Started implementation of the replay system.

---
+++ Assets/Scripts/MyReplay.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyReplay : MonoBehaviour {
+
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public struct MyKeyframe
+{
+    private float _frameTime;
+    private Vector3 _position;
+    private Quaternion _rotation;
+
+    public MyKeyframe(float frameTime, Vector3 position, Quaternion rotation)
+    {
+        _frameTime = frameTime;
+        _position = position;
+        _rotation = rotation;
+    }
+
+    public float FrameTime
+    {
+        get
+        {
+            return _frameTime;
+        }
+
+        set
+        {
+            _frameTime = value;
+        }
+    }
+
+    public Vector3 Position
+    {
+        get
+        {
+            return _position;
+        }
+
+        set
+        {
+            _position = value;
+        }
+    }
+
+    public Quaternion Rotation
+    {
+        get
+        {
+            return _rotation;
+        }
+
+        set
+        {
+            _rotation = value;
+        }
+    }
+}
